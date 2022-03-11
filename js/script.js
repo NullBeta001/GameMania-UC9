5ea2ddac22d628644bbdd57ac05ea4e1a7f86f31
NullBeta001 <fabiooli221297@gmail.com>
2022-03-11 20:41:15 -0300

    Ajustes no código, adição do jQuery bo projeto, e conplementação do Java Script

--- js/script.js
+++ js/script.js
@@ -1,3 +1,45 @@
+$(document).ready(function() {
+
+    let slideAtual = 1;
+    let listaSlides = ["banner-primario", "banner-secundario", "banner-piloto"]
+
+    setInterval(mudarSlide, 1000)
+
+    function mudarSlide() {
+        console.log("Slide atual:", slideAtual);
+
+        //Remove o slide anterior
+        if (slideAtual > 0) {
+            $("#carrossel").removeClass(listaSlides[slideAtual - 1])
+        } else if (slideAtual == 0) {
+            $("#carrossel").removeClass(listaSlides[ (listaSlides.length) - 1])
+        }
+
+        //Adiciona o slide atual
+        $("#carrossel").addClass(listaSlides[slideAtual]);
+
+        //Indica o slide atual
+        slideAtual++
+
+        if(slideAtual > (listaSlides.length) - 1) {
+            slideAtual = 0;
+        }
+
+    
+    }
+
+    $("#barras").click(function() {
+       
+        if($("#menu").hasClass("menu-ativo") ) {
+            $("#menu").removeClass("menu-ativo")
+        } else {
+            $("#menu").addClass("menu-ativo")
+        }
+
+    })
+
+})
+
 function mostrarMenu() {
     let menu = document.getElementById("menu")
 
